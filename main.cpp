3acf204a2f85aaa62eafdb50248caa9d6d1cd018
MTRNord <mtrnord1@gmail.com>
2021-09-03 20:11:43 +0200

    add dummy output to make it not fail while testing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "boinc_api.h"
+#include "filesys.h"
+
+using namespace std;
+FILE* fp;
 
 int main() {
     int retval;
     char buf[256];
+    fp = fopen("out.txt", "a");
+
     BOINC_OPTIONS options;
     boinc_options_defaults(options);
     options.normal_thread_priority = true;
@@ -15,6 +21,8 @@ int main() {
         exit(retval);
     }
 
+    fprintf(fp, "test\n");
+    fflush(fp);
     boinc_finish(0);
     return 0;
 }
